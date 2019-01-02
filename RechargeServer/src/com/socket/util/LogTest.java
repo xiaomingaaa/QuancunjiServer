8f42adc279fe09b94bfccd6db9c1a5b6215f9cd6
“Youre <13693963350@163.com>
2019-01-02 17:05:10 +0800

    已经基本完成

---
+++ RechargeServer/src/com/socket/util/LogTest.java
@@ -0,0 +1,14 @@
+package com.socket.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LogTest {
+
+	@Test
+	void test() {
+		Log.writeToRecharge("ceshi1");
+	}
+
+}
